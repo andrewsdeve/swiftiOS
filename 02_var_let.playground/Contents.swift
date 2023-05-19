6bc99d2bc8c551959eeeb3b9f9e2215706c72295
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2023-05-18 21:34:44 -0500

    Variables

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let numeroMaximoDeIntentos = 3
+var intentos = 0
+
+var x = 0.1, y = 0.3, z = 7.2
+
+var welcomeMessage : String
+
+welcomeMessage = "Hola que tal"
+
+var red,green, blue : Double
+
+let π = 3.141595
+
+
+print("El numero de intenttos es \( intentos) de un total disponibnle de: \( numeroMaximoDeIntentos)")
+
+// Esto es un comentario
+
+/* Esto es un comentario de varias
+ lineas y en el cual puedo escribir multiples lineas*/
+
+
+/* este es un comentario mulilinea
+ 
+ /* este es un comentario anidado*/
+ este es otro comewntario */
+
+
